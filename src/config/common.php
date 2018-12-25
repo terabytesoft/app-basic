256e0b8ecde10c20d0a83cb5e247a5dedee37715
terabytesoftw <terabytesoftw@gmail.com>
2018-12-25 17:14:57 -0400

    Aplication Config - (#1)

---
+++ src/config/common.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ *
+ *        @link: https://github.com/terabytesoft/app-basic
+ *      @author: Wilmer Arámbula <terabytesoftw@gmail.com>
+ *   @copyright: (c) TERABYTE SOFTWARE SA
+ *       @views: config[common]
+ *       @since: 0.0.1
+ *         @yii: 3.0
+ **/
+
+return [
+    'app' => [
+        'basePath' => dirname(__DIR__) . '/src',
+        'controllerNamespace' => \app\basic\commands::class,
+        /*
+        'controllerMap' => [
+            'fixture' => [ // Fixture generation command line.
+                '__class' => 'yii\faker\FixtureController',
+            ],
+        ],
+        */
+    ],
+    'logger' => [
+        '__construct()' => [
+            'targets' => [
+                [
+                    '__class' => yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'cache' => [
+        '__class' => yii\cache\Cache::class,
+        'handler' => [
+            '__class' => yii\cache\FileCache::class,
+            'keyPrefix' => 'my-project',
+        ],
+    ],
+    'mailer' => [
+        '__class' => yii\swiftmailer\Mailer::class,
+    ],
+    'db' => array_filter([
+        '__class' => yii\db\Connection::class,
+        'dsn' => $params['db.dsn'],
+        'username' => $params['db.username'],
+        'password' => $params['db.password'],
+        'enableSchemaCache' => defined('YII_ENV') && YII_ENV !== 'dev',
+    ]),
+    'translator' => [
+        'translations' => [
+            'yii-base-web' => [
+                '__class' => yii\i18n\PhpMessageSource::class,
+                'sourceLanguage' => 'en-US',
+                'basePath' => '@yii/app/messages',
+            ],
+        ],
+    ],
+];
---
+++ src/config/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ *
+ *        @link: https://github.com/terabytesoft/app-basic
+ *      @author: Wilmer Arámbula <terabytesoftw@gmail.com>
+ *   @copyright: (c) TERABYTE SOFTWARE SA
+ *       @views: config[web]
+ *       @since: 0.0.1
+ *         @yii: 3.0
+ **/
+
+return [
+    'app' => [
+        'controllerNamespace' => yii\app\controllers::class,
+    ],
+    'user' => [
+        'identityClass' => yii\app\models\User::class, // User must implement the IdentityInterface
+    ],
+    'request' => [
+        'enableCookieValidation' => false,
+        // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+        'cookieValidationKey' => '',
+    ],
+    'assetManager' => [
+        'appendTimestamp' => true,
+    ],
+    'urlManager' => [
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+    ],
+];
