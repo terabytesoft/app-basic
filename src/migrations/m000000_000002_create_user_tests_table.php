916f278fdc1c42bf58288177d0bef137ab6c216e
terabytesoftw <terabytesoftw@gmail.com>
2019-01-03 18:35:52 -0400

    Fix scrutinizer issues - (#2)

--- src/migrations/m000000_000002_create_user_tests_table.php
+++ src/migrations/m000000_000002_create_user_tests_table.php
@@ -12,8 +12,6 @@ class m000000_000002_create_user_tests_table extends Migration
      */
     public function up()
     {
-		$tableOptions = null;
-
 		if ($this->db->driverName === 'mysql') {
 			// http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
 			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
